a7483ac9b8b0bd890fb3416fd0ea997cdd12b6c7
Audrey Jiang <ajiang@lcs.on.ca>
2023-04-20 09:20:43 -0400

    Initial Commit

---
+++ CuteDogs/CuteDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuteDogsApp.swift
+//  CuteDogs
+//
+//  Created by Audrey Jiang on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct CuteDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
